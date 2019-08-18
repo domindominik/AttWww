b9a596a96679a0f189f74593e284af10c9376c2b
dominik <dominwro@gmail.com>
2019-08-18 20:45:54 +0200

    Initial commit.

---
+++ src/main/java/att/AttWwwApplication.java
@@ -0,0 +1,15 @@
+package att;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttWwwApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AttWwwApplication.class, args);
+	}
+
+}
---
+++ src/main/java/att/ServletInitializer.java
@@ -0,0 +1,6 @@
+package att;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+}
