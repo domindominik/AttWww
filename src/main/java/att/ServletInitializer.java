82f431c0769ee7f45cdb80eaad5dc477279c78a9
dominik <dominwro@gmail.com>
2019-08-18 20:54:26 +0200

    Add ServletInitializer class.

--- src/main/java/att/ServletInitializer.java
+++ src/main/java/att/ServletInitializer.java
@@ -1,6 +1,12 @@
 package att;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer
+{
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(AttWwwApplication.class);
+    }
 }
